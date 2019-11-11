f57802b6604930cab3463f9d66cc143f8058aa4c
Monica Tamaru Jones <mntamaru@gmail.com>
2019-11-11 19:09:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,8 +4,11 @@ function findMinAndRemove(array){
   let min = array[0];
   let minIndex = 0;
   
+  // loop through elements in array
   for (let i=0; i < array.length; i++) {
+    let currItem = array[i];
     
+    if (arr[])
   }
   
 
